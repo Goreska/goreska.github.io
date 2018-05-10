205ca6aa070c2e0d6d8b2b1e9601016283eb0036
Goreska <38164908+Goreska@users.noreply.github.com>
2018-05-10 20:48:10 +0300

    Alpha Desktop v2.1

--- nlalmaty/js/common.js
+++ nlalmaty/js/common.js
@@ -5,4 +5,24 @@ $(function() {
  		 pause: false,
  		 fade: true
 	});
+
+	/*Создаю переменную которая записана на сайте*/
+	var $grid = $('.grid').isotope({
+		itemSelector: '.grid-item',
+	  	layoutMode: 'fitRows'
+	});
+	/*Создаю фильтр который привязан к кнопкам, кликает по функции*/
+	$('.filter button').on("click", function() {
+		var value = $(this).attr('data-name');
+		$grid.isotope({
+		filter: value
+		});
+	})
+
+	
+
 });
+
+
+/* itemSelector: '.grid-item',
+	  layoutMode: 'fitRows'*/
