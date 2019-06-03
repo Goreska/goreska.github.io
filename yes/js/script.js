9752937c469917f0168c15f4ca6d6d72e9a91cec
Vladimir <38164908+Goreska@users.noreply.github.com>
2019-06-03 19:54:53 +0300

    Update modal form

--- yes/js/script.js
+++ yes/js/script.js
@@ -13,4 +13,30 @@ var mySwiper = new Swiper('.swiper-container', {
   },
 });
 
-new WOW().init();
+wow = new WOW ({
+  mobile: false
+}
+);
+
+wow.init();
+
+var modal = document.querySelector("#call-modal"),
+    modalOverlay = document.querySelector("#modal-overlay"),
+    closeButton = document.querySelector("#modal-close"),
+    openButton = document.querySelector("#open-modal");
+    openButtonFooter = document.querySelector("#open-modal-footer");
+
+closeButton.addEventListener("click", function(){
+  modal.classList.toggle("open");
+  modalOverlay.classList.toggle("open");
+});
+
+openButton.addEventListener("click", function(){
+  modal.classList.toggle("open");
+  modalOverlay.classList.toggle("open");
+});
+
+openButtonFooter.addEventListener("click", function(){
+  modal.classList.toggle("open");
+  modalOverlay.classList.toggle("open");
+});
